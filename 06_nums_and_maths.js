c538702f0e22c0cb1035bd669cce78f0c8ab4762
raje1966 <rajendra_kulkarni1@rediffmail.com>
2023-12-10 13:01:20 +0530

    Adding files to 'basic-javascript' on github

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+// const otherNumber = 232.2998
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++ MATHS +++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.sqrt(52));
+// console.log(Math.min(4, 5, 2, -1));
+// console.log(Math.max(6, 7, 8, 2));
+
+// console.log(Math.random()); //random value always comes between 0 and 1;
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+

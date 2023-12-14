272adc20633dc774a0cc60d779eae68d2dd33f4d
raje1966 <rajendra_kulkarni1@rediffmail.com>
2023-12-14 13:47:58 +0530

    adding more files to github

---
+++ 11_arrays2.js
@@ -0,0 +1,28 @@
+const marvel_heroes = ["Thor", "Ironman", "Spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes);
+// console.log(allHeroes);
+
+// const all_new_Heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_Heroes);
+
+const another_Array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_Array.flat(Infinity);
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Rajendra"));
+
+// console.log(Array.from("Rajendra"));
+// console.log(Array.from({ name: "Rajendra" }));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));

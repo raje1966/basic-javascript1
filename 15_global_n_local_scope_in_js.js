b8b7a842955222dbfc75eab5f3862b31df0d7196
raje1966 <rajendra_kulkarni1@rediffmail.com>
2023-12-16 15:03:19 +0530

    Adding more files to github

---
+++ 15_global_n_local_scope_in_js.js
@@ -0,0 +1,9 @@
+let a = 10
+const b = 20
+var c = 30
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Any values posted in the {} is within the scope of a braces.  Values outside the {} are in global scope and values inside the {} are block scope or local scope.

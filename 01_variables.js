14dbb456f0feb2302700c6978debedd84717ebae
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-17 06:28:01 +0530

    Adding '01_variables.js' to github

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 13456;
+let accountEmail = "raje@google.com";
+var accountPassword = "343333"
+accountCity = "Pune"
+
+// accountId = 33333;
+// console.log(accountId);
+// const variable values cannot be changed.
+
+accountEmail = "raj@gmail.com";
+accountPassword = "88888"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);

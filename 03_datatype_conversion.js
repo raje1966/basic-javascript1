d254fee3e7318eea66bbd7804d74a96b4686f75a
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-17 15:50:42 +0530

    Adding more files to github

---
+++ 03_datatype_conversion.js
@@ -0,0 +1,24 @@
+let score = "raj";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let numberInValue = Number(score);
+// console.log(typeof numberInValue);
+// console.log(numberInValue);
+
+// "33" => 33
+// "33abc" => NaN
+// true =1, false = 0
+
+let isLoggedIn = "raj";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "raj" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);

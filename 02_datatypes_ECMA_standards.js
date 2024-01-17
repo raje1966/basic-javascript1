509045298e6b60a2131cf178d864508258a83aa3
raje1966 <rajendra_kulkarni1@rediffmail.com>
2024-01-17 10:18:20 +0530

    Adding '02_datatypes_ECMA_standards.js' to github

---
+++ 02_datatypes_ECMA_standards.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version.
+// alert(3 + 3) //alert cannot be used in nodejs, only in browser.
+console.log(3
+    + 3) //do not put bad codes like this. code readability should be high.
+console.log("Raj") 
+
+//number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true or false
+//null => standalone value
+//undefined => no value assigned.
+//symbol =>
+
+console.log(typeof "raj");
+console.log(typeof null);
